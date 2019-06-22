c00816e93c20bbfe6b64bdf567613c4f582934ac
Zac Nowicki <zachnowicki@gmail.com>
2019-06-22 01:07:25 -0400

    Add Team related structs

--- src/discordcr/mappings/oauth2.cr
+++ src/discordcr/mappings/oauth2.cr
@@ -14,7 +14,8 @@ module Discord
       rpc_origins: Array(String)?,
       bot_public: Bool,
       bot_require_code_grant: Bool,
-      owner: User
+      owner: User,
+      team: Team?
     )
 
     # Produces a CDN URL for this application's icon in the given `format` and `size`
@@ -23,6 +24,34 @@ module Discord
       if icon = @icon
         CDN.application_icon(id, icon, format, size)
       end
+    end
+  end
+
+  struct Team
+    JSON.mapping(
+      owner_user_id: Snowflake,
+      members: Array(TeamMember),
+      id: Snowflake,
+      name: String,
+      icon: String?
+    )
+  end
+
+  struct TeamMember
+    JSON.mapping(
+      membership_state: TeamMembershipState,
+      team_id: Snowflake,
+      user: User,
+      permissions: Array(String)
+    )
+  end
+
+  enum TeamMembershipState : UInt8
+    Invited  = 1
+    Accepted = 2
+
+    def self.new(parser : JSON::PullParser)
+      TeamMembershipState.new(parser.read_int.to_u8)
     end
   end
 end
